6fbdfb61a53c34ff8af77d3b3fe43a2e12669fcc
heikkinenlaura <77092247+heikkinenlaura@users.noreply.github.com>
2022-05-09 18:58:45 +0300

    colliders, pause, clown

--- targethunt/Assets/Scripts/UI/ButtonHandler.cs
+++ targethunt/Assets/Scripts/UI/ButtonHandler.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
+    public GameObject panel;
     private void Start()
     {
         Time.timeScale = 0;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            PauseGame();
     }
     public void QuitGame()
     {
@@ -15,5 +22,20 @@ public class ButtonHandler : MonoBehaviour
     public void StartGame()
     {
         Time.timeScale = 1;
+        panel.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        panel.SetActive(true);
+    }
+    public void ReplayGame()
+    {
+
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
